07b00c5e3dd453db39ea22e7b0b7417f2c2943c5
Daniel <djblancoh@gmail.com>
2020-08-16 21:20:05 -0500

    finish clase 09, creacion de clase servlet

---
+++ platziweb/src/main/java/com/platzi/platziweb/servlet/ServletLogin.java
@@ -0,0 +1,22 @@
+package com.platzi.platziweb.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/***
+ * Servlet para login
+ */
+@WebServlet("/ServletLogin")
+public class ServletLogin extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
